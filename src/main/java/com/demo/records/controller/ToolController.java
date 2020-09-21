19cadb7ffeebb1fa98b7387efc8722f25f0a9ca0
jiayanfighting <jiayanfighting@163.com>
2020-09-21 17:58:05 +0800

    add translate function

---
+++ src/main/java/com/demo/records/controller/ToolController.java
@@ -0,0 +1,57 @@
+package com.demo.records.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.demo.records.translate.TransApi;
+import com.demo.records.utils.Result;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/tool")
+public class ToolController {
+    // 在平台申请的APP_ID 详见 http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+    private static final String APP_ID = "20200921000570393";
+    private static final String SECURITY_KEY = "CY7SP6WlPm7sf0Q4WZRc";
+
+    @ResponseBody
+    @RequestMapping("/removeLine")
+    public Result removeLine(String content){
+        System.out.println(content);
+        StringBuilder sb = new StringBuilder();
+        String[] lineList = content.split("\n");
+        for (String s:lineList){
+            sb.append(s);
+            sb.append(" ");
+        }
+        Result res = new Result();
+        String translatedContent = translate(sb.toString());
+        res.put("data",sb.toString());
+        res.put("translatedData",translatedContent);
+        return res;
+    }
+
+
+    public String translate(String query) {
+        TransApi api = new TransApi(APP_ID, SECURITY_KEY);
+        String json = api.getTransResult(query, "en", "zh");
+        System.out.println(json);
+        JSONObject jsonObject = JSONObject.parseObject(json);
+
+        if (jsonObject.containsKey("trans_result")){
+            Object o = jsonObject.get("trans_result");
+                if (o instanceof JSONArray){
+                Object jo = ((JSONArray) o).get(0);
+                if (jo instanceof JSONObject){
+                    String res = ((JSONObject) jo).getString("dst");
+                    System.out.println(res);
+                    return res;
+                }
+            }
+        }
+        return "error";
+    }
+}
